3eda6867f804183466971fd4422c2013865e0577
dido <davide.neri@unipi.it>
2018-02-07 16:53:07 +0100

    run script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/di-unipi-socc/DockerAnalyser.git
 
 rm -r DockerAnalyser/deploy-package-ds
-cp -r deploy-package-dm  ./DockerAnalyser/
+cp -r deploy-package-ds  ./DockerAnalyser/
 
 docker-compose -f DockerAnalyser/docker-compose.yml build --build-arg  DEPLOY_PACKAGE_PATH=/deploy-package-ds scanner
---
+++ run.sh
@@ -0,0 +1,1 @@
+docker-compose -f DockerAnalyser/docker-compose.yml  -p docker-snaptrend up -d
